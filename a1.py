1cde2e50f03ee4dae6082365854dbe11742aa5f4
akKmmp <104196775+akKmmp@users.noreply.github.com>
2022-10-22 09:54:55 +0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -12,7 +12,7 @@ re = [0,0]
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #UPD接收
 udp.bind(('', 4000))                           # 为服务器绑定一个固定的地址，ip和端口
 a = 125
-go = 0.5                                                      #直行速度
+go = 1                                                      #直行速度
 #------------------------------------------------------------#
 
 #UPD接收
@@ -20,43 +20,82 @@ def cv_m():
     data,addr = udp.recvfrom(1024)
     #print(data.decode('utf-8') ) #打印接收的内容
     data = int(data)
-    if data >= 10:
-        re[0] = data
-    elif data >= 0 and data < 10:
-        re[1] = data
-    else:
-        re[0] = 0
-        re[1] = 0
-    return re         #返回值
+    return data         #返回值
 
-#主函数
-def main():
+def no_1():         #倾倒
     motion_time = 0
-    while True:
-        Middle = cv_m()
-        print(Middle[0])
-        #print(Middle[1])
-        if (Middle[0] == 11):
+    while motion_time <= 50:
+        time.sleep(0.1)
+        motion_time += 1
+        if (motion_time >= 1 and motion_time < 5):              #停止
             state = walk_1.stop_walk()
+        elif(motion_time >= 5 and motion_time < 30):            #直行
+            state = walk_1.forward_walk(0.7,-0.05)
+        if (motion_time >= 30 and motion_time < 35):            #停止，防止卡脚
+            state = walk_1.stop_walk()
+        elif (motion_time >= 35 and motion_time < 50):          #倾倒
+            state = walk_1.robot_pose(1.9,0.0,0.0,0.0)
+        print(motion_time)
+        cv_m()
 
+def no_2():                         #爬楼梯
+    motion_time = 0
+    while motion_time < 35:
+        time.sleep(0.1)
+        motion_time += 1
+        if(motion_time >= 1 and motion_time < 13):
+            state=walk_1.rightRotate_walk(1,-0.7,-0.1)
+        if(motion_time >= 13):
+            state=walk_1.robot_climb()
+        print(motion_time)
+        cv_m()
 
+def no_3():
+    motion_time = 0
+    while motion_time < 45:
+        time.sleep(0.1)
+        motion_time += 1
+        if motion_time >= 0 and motion_time <= 15:
+            state = walk_1.forward_walk(go,0.1)
+        elif motion_time > 15 and motion_time <= 25:
+            state = walk_1.leftyaw_walk(0)
+        elif motion_time > 25 and motion_time <= 35:
+            state = walk_1.forward_walk(go,0.1)
+        elif motion_time > 35:
+            state = walk_1.rightyaw_walk(0)
+        print(motion_time)
+        cv_m()
 
-        #运动判断
-        elif(Middle[1] == 0): 
+#主函数c
+def main():
+    while True:
+        Middle = 999
+        Middle = cv_m()
+        print("视觉返回值",Middle)
+        #print(Middle[1])
+        if(Middle == 11):       #倾倒检测
+           no_1()
+        elif(Middle == 12):#爬楼梯
+            no_2()
+        elif(Middle == 13): #避障
+            no_3()
+        elif(Middle == 0): 
             state = walk_1.forward_walk(go,0.1)      #直行
         #向左修正，(直行速度，旋转，侧向)
-        elif(Middle[1] == -1):
-            state = walk_1.leftRotate_walk(go , 0.2 ,0.1)
-        elif(Middle[1] == -2):
+        elif(Middle == -1):
             state = walk_1.leftRotate_walk(go , 0.4 ,0.1)
+        elif(Middle == -2):
+            state = walk_1.leftRotate_walk(go , 0.6 ,0.1)
         #向右修正，(直行速度，旋转，侧向)
-        elif(Middle[1] == 1):
-            state = walk_1.rightRotate_walk(go, -0.2 ,-0.1)
-        elif(Middle[1] == 2):
-            state = walk_1. rightRotate_walk(go, -0.4 ,-0.1)
+        elif(Middle == 1):
+            state = walk_1.rightRotate_walk(go, -0.4 ,-0.1)
+        elif(Middle == 2):
+            state = walk_1. rightRotate_walk(go, -0.6 ,-0.1)
         
-        elif(Middle[1] == -3):
-            state = walk_1. forward_walk((0.4),-0.05)
-        elif(Middle[1] == 3):
-            state = walk_1. Robot_rightRotate()
+        # elif(Middle == -3):
+        #     state = walk_1. forward_walk((0.4),-0.05)
+        elif(Middle == 3):
+            state = walk_1. Robot_rightRotate()         #转向
+        else: 
+            state = walk_1.stop_walk()          #停止
 main()
---
+++ walk.py
@@ -0,0 +1,184 @@
+#!/usr/bin/python
+import sys
+import time
+from tkinter import N
+sys.path.append('/home/ak/文档/unitree_legged_sdk-3.3.2/build') # Edit the path to "build" folder on your computer
+import robot_interface_high_level as robot_interface #导入robot_interface_high_level模块时指定了别名robot_interface
+motion_time=0
+class Unitree_Robot():#定义一个类叫宇数机器人
+
+    unitree_robot = robot_interface.RobotInterface()#unitree_robot机器人接口
+    robot_state = robot_interface.HighState()#高层状态
+    
+     
+    def __init__(self):     #
+        self.mode = 0 #
+        self.gaitType = 0   #步态
+        self.speedLevel = 0
+        self.footRaiseHeight = 0.0
+        self.forwardSpeed = 0.0  #向前后
+        self.sidewaySpeed = 0.0  #向左右移动
+        self.rotateSpeed = 0.0   #向左右旋转
+        self.bodyHeight = 0.0   #调整站立高度指令（-1，+1；0.3，0.45 ）
+        self.yaw = 0.0          #偏航（-1，+1；+-28）
+        self.pitch = 0.0        #俯仰（+-20）
+        self.roll = 0.0         #横滚（+-20）
+        self.quit_dance_time = 0 
+
+    def cmd_init(self):
+        self.mode = 0
+        self.gaitType = 0
+        self.speedLevel = 0
+        self.footRaiseHeight = 0.0
+        self.forwardSpeed = 0.0
+        self.sidewaySpeed = 0.0
+        self.rotateSpeed = 0.0
+        self.bodyHeight = 0.0
+        self.yaw = 0.0
+        self.pitch = 0.0
+        self.roll = 0.0
+        self.quit_dance_time = 0
+    
+    def send_UDP(self):
+        self.unitree_robot.UDPSend()    #执行udpsend功能
+
+    def recv_UDP(self):
+        self.unitree_robot.UDPRecv()    #执行udprecv功能
+        
+    def robot_control(self):
+        self.unitree_robot.robotControl(self.mode, self.gaitType, self.speedLevel,    
+                                    self.footRaiseHeight, self.bodyHeight, 
+                                    self.roll, self.pitch, self.yaw,
+                                    self.forwardSpeed, self.sidewaySpeed, self.rotateSpeed) #机器人控制
+    
+    
+
+
+    def robot_walking(self, gaitType = 1, forwardSpeed = 0.0, sidewaySpeed = 0.0, 
+                      rotateSpeed = 0.0, speedLevel = 0, bodyHeight = 0.0, footRaiseHeight = 0.0):#机器人行走
+        self.cmd_init()
+        self.gaitType = gaitType #=1
+        self.speedLevel = speedLevel #=0.0
+        self.footRaiseHeight = footRaiseHeight  #=0.0
+        self.forwardSpeed = forwardSpeed    #=0.0
+        self.sidewaySpeed = sidewaySpeed    #=0.0
+        self.rotateSpeed = rotateSpeed      #=0.0
+        self.bodyHeight = bodyHeight        #=0.0
+        self.mode = 2 #进入模式2
+        robot_state = self.unitree_robot.getState()#getshate将walking数值记录下来
+        self.recv_UDP()
+        self.robot_control()  #运行robot_control
+        self.send_UDP()
+        return robot_state  #将robot_state值返回终端
+    def stop_walk(self):#停止走路
+        self.cmd_init()
+        self.gaitType = 0
+        self.mode =2
+        robot_state = self.unitree_robot.getState()#getshate将walking数值记录下来
+        self.recv_UDP()
+        self.robot_control()  #运行robot_control
+        self.send_UDP()
+        return robot_state  #将robot_state值返回终端     
+    def leftyaw_walk(self,forwardSpeed ):#左斜
+        self.cmd_init()
+        self.gaitType = 1
+        self.forwardSpeed = forwardSpeed
+        self.sidewaySpeed = 0.6
+        self.mode =2
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+        return robot_state  
+    def rightyaw_walk(self, forwardSpeed):#右斜
+        self.cmd_init()
+        self.gaitType = 1
+        self.forwardSpeed = forwardSpeed
+        self.sidewaySpeed = -0.2
+        self.mode =2
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+        return robot_state        
+    def leftRotate_walk(self,forwardSpeed,rotateSpeed,sidewaySpeed):#左转（走）
+        self.cmd_init()
+        self.gaitType = 1
+        self.rotateSpeed =rotateSpeed
+        self.sidewaySpeed = sidewaySpeed
+        self.forwardSpeed = forwardSpeed
+        self.mode =2
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+        self.rotateSpeed = -0.6
+    def rightRotate_walk(self,forwardSpeed,rotateSpeed,sidewaySpeed):#右转（走）
+        self.cmd_init()
+        self.gaitType = 1
+        self.rotateSpeed = rotateSpeed
+        self.sidewaySpeed = sidewaySpeed
+        self.forwardSpeed = forwardSpeed
+        self.mode =2
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+    def Robot_leftRotate(self):#左转
+        self.cmd_init()
+        self.gaitType =1
+        self.rotateSpeed = 2
+        self.mode = 2
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+    def Robot_rightRotate(self):#右转
+        self.cmd_init()
+        self.forwardSpeed = 0.2
+        self.gaitType =1
+        self.rotateSpeed =-1.8
+        self.mode = 2
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+    def forward_walk(self,forwardSpee,rotateSpeed):#直行
+        self.gaitType = 1
+        self.forwardSpeed =forwardSpee
+        self.rotateSpeed = rotateSpeed
+        self.mode = 2
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+    def a1_yaw(self):
+        self.mode=2
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+    def robot_pose(self, roll, pitch, yaw, bodyHeight):#机器人动作
+        self.cmd_init()
+        self.bodyHeight = bodyHeight    #高度
+        self.yaw = yaw
+        self.pitch = pitch
+        self.roll = roll
+        self.mode = 1
+        robot_state = self.unitree_robot.getState()####
+        self.recv_UDP()     #####
+        self.robot_control()#####
+        self.send_UDP()####
+        return robot_state #####
+    def robot_climb(self):          #爬楼梯
+        self.mode=2
+        self.gaitType = 3
+        self.forwardSpeed = 0.7
+        self.footRaiseHeight = 0.5
+        self.sidewaySpeed = 0.05
+        self.rotateSpeed = 0.01
+        robot_state = self.unitree_robot.getState()
+        self.recv_UDP()
+        self.robot_control()
+        self.send_UDP()
+        return robot_state
